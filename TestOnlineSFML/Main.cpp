f0b4b379e113b563f036fd26c8735c0a867beb2a
guillaume volgyesi <guillaumevolgyesi@gmail.com>
2019-09-23 10:14:08 +0200

    Backup example

--- TestOnlineSFML/Main.cpp
+++ TestOnlineSFML/Main.cpp
@@ -37,9 +37,11 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "Packets");
 
 	sf::Vector2f prevPosition;
-	sf::Vector3f p2Position;
+	sf::Vector2f p2Position;
 
 	socket.setBlocking(false);
+
+	bool update = false;
 
 	while (window.isOpen())
 	{
@@ -50,18 +52,41 @@ int main()
 			{
 				window.close();
 			}
+			else if (Event.type == sf::Event::GainedFocus)
+			{
+				update = true;
+			}
+			else if (Event.type == sf::Event::LostFocus)
+			{
+				update = false;
+			}
 		}
 
 		prevPosition = rect1.getPosition();
+		if (update)
+		{
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+				rect1.move(0.5f, 0.0f);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+				rect1.move(-0.5f, 0.0f);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+				rect1.move(0.0f, -0.5f);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+				rect1.move(0.0f, 0.5f);
+		}
+		sf::Packet packet;
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-			rect1.move(0.5f, 0.0f);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-			rect1.move(-0.5f, 0.0f);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-			rect1.move(0.0f, -0.5f);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-			rect1.move(0.0f, 0.5f);
+		if (prevPosition != rect1.getPosition())
+		{
+			packet << rect1.getPosition().x << rect1.getPosition().y;
+			socket.send(packet);
+		}
+
+		socket.receive(packet);
+		if (packet >> p2Position.x >> p2Position.y)
+		{
+			rect2.setPosition(p2Position);
+		}
 
 		window.draw(rect1);
 		window.draw(rect2);
